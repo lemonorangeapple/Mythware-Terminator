9180cb0291d8079e72951ec96bfa8946d3b14eec
lemonorangeapple <jcj1947725596@hotmail.com>
2023-11-12 13:45:52 +0800

    update

--- Mythware-Terminator.cpp
+++ Mythware-Terminator.cpp
@@ -1,9 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
 using namespace std;
-#define LONG_FULL_SCREEN -1778384896
-#define LONG_WINDOW -1764818944
 
 DWORD main_pid = 0;
 DWORD hook_pid = 0;
@@ -35,6 +33,7 @@ BOOL FindProcessPid(LPCSTR ProcessName, DWORD& dwPid) {
         }
 
     } while (Process32Next(hProcessSnap, &pe32));
+
     CloseHandle(hProcessSnap);
     return bRet;
 }
@@ -72,48 +71,60 @@ void ResumeProcess(DWORD process_id) {
 LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     return FALSE;
 }
+
 DWORD WINAPI KeyHookThreadProc(LPVOID lpParameter) {
-    kbdHook = (HHOOK)SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)HookProc, GetModuleHandle(NULL), 0);
-    if (kbdHook != NULL) {
-        UnhookWindowsHookEx(kbdHook);
+    while (true) {
+        kbdHook = (HHOOK)SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)HookProc, GetModuleHandle(NULL), 0);
+        Sleep(50);
+        if (kbdHook != NULL) {
+            UnhookWindowsHookEx(kbdHook);
+        }
     }
     return 0;
 }
 
 DWORD WINAPI MouseHookThreadProc(LPVOID lpParameter) {
-    mseHook = (HHOOK)SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC)HookProc, GetModuleHandle(NULL), 0);
-    if (mseHook != NULL) {
-        UnhookWindowsHookEx(mseHook);
+    while (true) {
+        mseHook = (HHOOK)SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC)HookProc, GetModuleHandle(NULL), 0);
+        ClipCursor(0);
+        Sleep(50);
+        if (mseHook != NULL) {
+            UnhookWindowsHookEx(mseHook);
+        }
     }
     return 0;
 }
+
+DWORD WINAPI WindowHookThreadProc(LPVOID lpParameter) {
+	while (true) {
+		if (!IsWindow(hBoardWindow)) {
+			hBoardWindow = FindWindow(NULL, "ÆÁÄ»¹ã²¥");
+			if (hBoardWindow == NULL) {
+				hBoardWindow = FindWindow(NULL, "¹²ÏíÆÁÄ»");
+			}
+			if (hBoardWindow == NULL) {
+				hBoardWindow = FindWindow(NULL, "BlackScreen Window");
+			}
+		}
+		ShowWindow(hBoardWindow, SW_MINIMIZE);
+		Sleep(50);
+	}
+	return 0;
+}
+
 
 void init();
 
 void Suspend_service() {
     SuspendProcess(main_pid);
-    Sleep(3000);
+    Sleep(1500);
     system("cls");
     init();
 }
 
 void Resume_service() {
     ResumeProcess(main_pid);
-    Sleep(3000);
-    system("cls");
-    init();
-}
-
-void Unlock_keyboard() {
-	CreateThread(NULL, 0, KeyHookThreadProc, NULL, 0, NULL);
-	Sleep(3000);
-    system("cls");
-    init();
-}
-
-void Unlock_mouse() {
-	CreateThread(NULL, 0, MouseHookThreadProc, NULL, 0, NULL);
-	Sleep(3000);
+    Sleep(1500);
     system("cls");
     init();
 }
@@ -122,16 +133,12 @@ void init() {
     int n;
     cout << "1. Suspend service"<< endl;
     cout << "2. Resume service" << endl;
-    cout << "3. Unlock keyboard" << endl;
-    cout << "4. Unlock mouse" << endl;
-    cout << "5. Exit" << endl;
+    cout << "3. Exit" << endl;
     cin >> n;
     switch(n) {
         case 1: Suspend_service(); break;
         case 2: Resume_service(); break;
-        case 3: Unlock_keyboard(); break;
-        case 4: Unlock_mouse(); break;
-        case 5: exit(0); break;
+        case 3: exit(0); break;
         default: system("cls"); init();
     }
 }
@@ -140,12 +147,16 @@ int main() {
     SetConsoleTitle("Mythware Terminator");
     HWND hWnd = ::GetForegroundWindow();
     SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_TOOLWINDOW);
+    ShowWindow(hWnd, SW_MINIMIZE);
 	::SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 100, 100, SWP_NOMOVE | SWP_NOSIZE | SWP_NOREPOSITION);
     FindProcessPid("StudentMain.exe", main_pid);
-    if (main_pid == NULL) { 
+    if (main_pid == NULL) {
     	ShowWindow(hWnd, SW_HIDE);
         MessageBox(hWnd, "¼«ÓòÎ´ÔËÐÐ£¡", "Mythware Terminator", MB_OK);
         return 0;
     }
-	init();
+    CreateThread(NULL, 0, KeyHookThreadProc, NULL, 0, NULL);
+    CreateThread(NULL, 0, MouseHookThreadProc, NULL, 0, NULL);
+    CreateThread(NULL, 0, WindowHookThreadProc, NULL, 0, NULL);
+    init();
 }
